5d64907594aedac036241568abbf8e77f9d9e160
ace68723 <yichenghuang@yichengs-Mac-mini-2.local>
2014-12-26 02:49:44 -0500

    add result function

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -6,10 +6,18 @@ model.controller('loginCtrl', function (loginService) {
 					}
 		this.submit = function  () {
 		 this.response = loginService.post(this.user)
-			console.log(this.response)
+			// console.log(this.response)
+			if(this.response.result == '1'){
+				window.localStorage.setItem("sv_rid", this.response.rid);
+				window.localStorage.setItem("sv_token", this.response.token);
+			}else{
+				alert(this.response.error_msg)
+			}
+
 			return {
 					response: this.response,
-					result: this.response.result
+					result: this.response.result,
+					error_msg: this.response.error_msg
 					}
 		}
 
--- src/app/login/login.spec.js
+++ src/app/login/login.spec.js
@@ -51,5 +51,35 @@ describe('loginService',function  () {
               expect(loginCtrl.user.username).toBe('');
               expect(loginCtrl.submit().response).toBe('ERROR!');
         })
+     it('should save data in local', function  () {
+            var respond = {
+                          'result': '1',
+                          'rid': '3',
+                          'uid': '2',
+                          'token': 'sdfkdkqikdkkqe',
+                          'error_msg': 'pasword'}
+            
+             $httpBackend.expectPOST('http://chanmao.ca/?r=%20rrclient/login', user).respond(201, respond);
+              loginService.post(user);
+              $httpBackend.flush();
+    
+              expect(loginCtrl.submit().result).toBe('1');
+              expect( window.localStorage.getItem("sv_rid")).toBe('3');
+              expect(window.localStorage.getItem("sv_token")).toBe('sdfkdkqikdkkqe');
+     })
+     it('should alert error message', function  () {
+            var respond = {
+                          'result': '0',
+                          'rid': '3',
+                          'uid': '2',
+                          'token': 'sdfkdkqikdkkqe',
+                          'error_msg': 'pasword'}
+            
+             $httpBackend.expectPOST('http://chanmao.ca/?r=%20rrclient/login', user).respond(201, respond);
+              loginService.post(user);
+              $httpBackend.flush();
+    
+              expect(loginCtrl.submit().error_msg).toBe('pasword');
+     })
       
 })
