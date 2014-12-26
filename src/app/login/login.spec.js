853739aad362f1541f130b3dc54c98d2f259d982
ace68723 <yichenghuang@yichengs-Mac-mini-2.local>
2014-12-26 02:22:40 -0500

    controller test

--- src/app/login/login.spec.js
+++ src/app/login/login.spec.js
@@ -1,1 +1,55 @@
 'use strict';
+
+
+describe('loginService',function  () {
+     var loginService, loginCtrl, $controller, $httpBackend;
+     var user = {'userName': 'test',
+                    'password': 'test123'};
+     beforeEach(module("angular-login-model"));
+     beforeEach(module("LoginCtrl-model"));
+     beforeEach(inject(function  ($controller, _loginService_, _$httpBackend_) {
+          loginService = _loginService_;
+          loginCtrl = $controller("loginCtrl",{loginService:loginService})
+          $httpBackend = _$httpBackend_;
+
+     }));
+
+     it('should login success', function() {
+               
+                 var respond = {
+                          'result': 0,
+                          'rid': 1,
+                          'uid': 2,
+                          'token': 'sdfkdkqikdkkqe',
+                          'error_msg': 'pasword'}
+            
+             $httpBackend.expectPOST('http://chanmao.ca/?r=%20rrclient/login', user).respond(201, respond);
+              loginService.post(user);
+             // loginCtrl.submit()
+              $httpBackend.flush();
+
+              //expect(loginService.post(user)).toBe(respond);
+              expect(loginCtrl.user.username).toBe('');
+              expect(loginCtrl.submit().result).toBe(0);
+        })
+
+     it('should show error', function() {
+               
+                 var respond = {
+                          'result': 0,
+                          'rid': 1,
+                          'uid': 2,
+                          'token': 'sdfkdkqikdkkqe',
+                          'error_msg': 'pasword'}
+            
+             $httpBackend.expectPOST('http://chanmao.ca/?r=%20rrclient/login', user).respond(401, respond);
+              loginService.post(user);
+             // loginCtrl.submit()
+              $httpBackend.flush();
+
+              //expect(loginService.post(user)).toBe(respond);
+              expect(loginCtrl.user.username).toBe('');
+              expect(loginCtrl.submit().response).toBe('ERROR!');
+        })
+      
+})
